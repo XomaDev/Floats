b6dc63ee12850e8273e2c1c5e94d9d170c583c42
kumaraswamy <care.xoma@gmail.com>
2023-06-12 18:45:13 +0530

    Fix file receive problem

--- app/src/main/java/com/baxolino/apps/floats/core/files/FileRequest.kt
+++ app/src/main/java/com/baxolino/apps/floats/core/files/FileRequest.kt
@@ -6,7 +6,6 @@ import android.net.Uri
 import android.os.Build
 import android.util.Log
 import com.baxolino.apps.floats.core.ChannelInfo
-import com.baxolino.apps.floats.core.MultiChannelSystem
 import com.baxolino.apps.floats.core.TaskExecutor
 import com.baxolino.apps.floats.core.transfer.SocketUtils
 import com.baxolino.apps.floats.core.io.BitStream
@@ -33,18 +32,6 @@ class FileRequest(
       .toBytes()
     exec.writer.write(ChannelInfo.FILE_REQUEST_CHANNEL_INFO, requestData)
 
-    exec.registerOnCancel(localPort) {
-      // look for cancel requests from the receiver,
-      // this will invoke the native class at the end to
-      // stop sending data and eventually ending the service
-      Log.d(TAG, "Cancellation Requested")
-      context.sendBroadcast(Intent()
-        .apply {
-          action = FileRequestService.CANCEL_REQUEST_ACTION
-        })
-    }
-
-    // the uploading world will be executed by the foreground service
     val service = Intent(context, FileRequestService::class.java)
       .putExtra("file_uri", fileInput.toString())
       .putExtra("file_name", fileName)
@@ -54,6 +41,17 @@ class FileRequest(
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
       context.startForegroundService(service)
     else context.startService(service)
+
+    exec.register(localPort, forgetAfter = true) {
+      // it was a cancel request
+      // this will invoke the native class at the end to
+      // stop sending data and eventually ending the service
+      Log.d(TAG, "Cancellation Requested")
+      context.sendBroadcast(Intent()
+        .apply {
+          action = FileRequestService.CANCEL_REQUEST_ACTION
+        })
+    }
   }
 
   companion object {
