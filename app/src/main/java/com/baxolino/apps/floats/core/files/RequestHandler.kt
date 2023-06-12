60750176939e3c6a9e21151abc30bb61dfa1fb8a
kumaraswamy <care.xoma@gmail.com>
2023-06-12 18:46:07 +0530

    Kotlin migration

---
+++ app/src/main/java/com/baxolino/apps/floats/core/files/RequestHandler.kt
@@ -0,0 +1,47 @@
+package com.baxolino.apps.floats.core.files
+
+import com.baxolino.apps.floats.core.ChannelInfo
+import com.baxolino.apps.floats.core.MultiChannelStream
+import com.baxolino.apps.floats.core.TaskExecutor
+import com.baxolino.apps.floats.core.io.DataInputStream
+import com.baxolino.apps.floats.core.io.DataInputStream.ByteListener
+
+class RequestHandler(private val listener: (FileReceiver) -> Unit) {
+
+  private lateinit var reader: MultiChannelStream
+  private lateinit var exec: TaskExecutor
+
+  fun setReader(reader: MultiChannelStream, executor: TaskExecutor) {
+    this.reader = reader
+    this.exec = executor
+
+    init()
+  }
+
+  private fun init() {
+    val requests = DataInputStream()
+    requests.byteListener = ByteListener {
+      val port = requests.readInt32()
+
+      val lengthOfFile = requests.readInt32()
+
+      val nameStringLength = requests.readInt32()
+      val fileName = ByteArray(nameStringLength)
+
+      requests.read(fileName)
+      val name = String(fileName)
+
+      listener.invoke(
+        FileReceiver(
+          port,
+          name,
+          lengthOfFile
+        )
+      )
+      // true because, we do not need rest of the
+      // byte listen calls
+      true
+    }
+    reader.registerChannelStream(ChannelInfo.FILE_REQUEST_CHANNEL_INFO, requests)
+  }
+}
